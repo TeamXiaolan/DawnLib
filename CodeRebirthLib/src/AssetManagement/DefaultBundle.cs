a0f6c1593fcdf70c13a1676e525776916f799e56
LoafOrc <33192068+LoafOrc@users.noreply.github.com>
2025-06-14 22:26:18 +1000

    rename again

---
+++ CodeRebirthLib/src/AssetManagement/DefaultBundle.cs
@@ -0,0 +1,6 @@
+﻿namespace CodeRebirthLib.AssetManagement;
+public class DefaultBundle : AssetBundleLoader<DefaultBundle>
+{
+    protected DefaultBundle(CRMod mod, string filePath) : base(mod, filePath) {
+    }
+}
--- CodeRebirthLib/src/AssetManagement/DefaultBundleLoader.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CodeRebirthLib.AssetManagement;
-public class DefaultBundleLoader : AssetBundleLoader<DefaultBundleLoader>
-{
-    protected DefaultBundleLoader(CRMod mod, string filePath) : base(mod, filePath) {
-    }
-}
