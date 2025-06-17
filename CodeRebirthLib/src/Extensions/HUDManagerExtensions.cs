ca4e66901d655ede3c69c8f22cbc60238a33c820
LoafOrc <33192068+LoafOrc@users.noreply.github.com>
2025-06-17 12:27:01 +1000

    allow sending warning tips with type enum, might change?

--- CodeRebirthLib/src/Extensions/HUDManagerExtensions.cs
+++ CodeRebirthLib/src/Extensions/HUDManagerExtensions.cs
@@ -5,6 +5,6 @@ public static class HUDManagerExtensions
 {
     public static void DisplayTip(this HUDManager instance, HUDDisplayTip displayTip)
     {
-        instance.DisplayTip(displayTip.Header, displayTip.Body);
+        instance.DisplayTip(displayTip.Header, displayTip.Body, displayTip.Type == HUDDisplayTip.AlertType.Warning);
     }
 }
--- CodeRebirthLib/src/Util/HUDDisplayTip.cs
+++ CodeRebirthLib/src/Util/HUDDisplayTip.cs
@@ -4,15 +4,24 @@ namespace CodeRebirthLib.Util;
 // todo: add other things that are used in a hud display tip.
 public class HUDDisplayTip : INetworkSerializable
 {
+    public enum AlertType
+    {
+        Hint,
+        Warning
+    }
+    
     private string _header, _body;
+    private AlertType _alertType;
 
     public string Header => _header;
     public string Body => _body;
+    public AlertType Type => _alertType;
     
-    public HUDDisplayTip(string header, string body)
+    public HUDDisplayTip(string header, string body, AlertType type = AlertType.Hint)
     {
         _header = header;
         _body = body;
+        _alertType = type;
     }
 
 
@@ -20,5 +29,6 @@ public class HUDDisplayTip : INetworkSerializable
     {
         serializer.SerializeValue(ref _header);
         serializer.SerializeValue(ref _body);
+        serializer.SerializeValue(ref _alertType);
     }
 }
