418904fd8455940592a7ebd7f6367e485a1149db
XuuXiao <ammaroaladel@Gmail.com>
2025-07-14 03:48:15 +0100

    edited clearing proximity nodes for smart agent navigator to not be a coroutine also chance script null checking also changelog

--- CodeRebirthLib/src/MiscScriptManagement/ChanceScript.cs
+++ CodeRebirthLib/src/MiscScriptManagement/ChanceScript.cs
@@ -1,8 +1,10 @@
+using System.Collections;
 using CodeRebirthLib.Util;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace CodeRebirthLib.MiscScriptManagement;
+
 [DefaultExecutionOrder(-999)]
 public class ChanceScript : MonoBehaviour
 {
@@ -14,9 +16,25 @@ public class ChanceScript : MonoBehaviour
 
     public void Awake()
     {
-        int randomNumber = CodeRebirthLibNetworker.Instance!.CRLibRandom.Next(0, 100) + 1;
+        if (CodeRebirthLibNetworker.Instance == null)
+        {
+            CodeRebirthLibPlugin.Logger.LogWarning($"CodeRebirthLibNetworker.Instance is null! I really hope you're starting up the round right now");
+            StartCoroutine(DelayRandomThing());
+            return;
+        }
+        int randomNumber = CodeRebirthLibNetworker.Instance.CRLibRandom.Next(0, 100) + 1;
         if (randomNumber > _chance)
             return;
+
+        _onChance.Invoke();
+    }
+    
+    private IEnumerator DelayRandomThing()
+    {
+        yield return new WaitUntil(() => CodeRebirthLibNetworker.Instance != null);
+        int randomNumber = CodeRebirthLibNetworker.Instance!.CRLibRandom.Next(0, 100) + 1;
+        if (randomNumber > _chance)
+            yield break;
 
         _onChance.Invoke();
     }
