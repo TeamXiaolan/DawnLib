b68e90acd8efd7af00a9947c4a16d001f86246e3
XuuXiao <ammaroaladel@Gmail.com>
2025-08-06 08:50:21 +0100

    items + dungeon todo

--- CodeRebirthLib/src/ContentManagement/LethalContent.cs
+++ CodeRebirthLib/src/ContentManagement/LethalContent.cs
@@ -5,6 +5,7 @@ using CodeRebirthLib.ContentManagement.Enemies;
 using CodeRebirthLib.ContentManagement.Items;
 using DunGen;
 using DunGen.Graph;
+using LethalLevelLoader;
 using Unity.Netcode;
 
 namespace CodeRebirthLib.ContentManagement;
@@ -122,15 +123,95 @@ public static class LethalContent
         
         public static Item SevenBall { get; private set; }
         public static Item Airhorn { get; private set; }
+        public static Item BabyKiwiEgg { get; private set; }
         public static Item Bell { get; private set; }
+        public static Item BeltBag { get; private set; }
         public static Item BigBolt { get; private set; }
+        public static Item Binoculars { get; private set; }
+        public static Item Boombox { get; private set; }
+        public static Item BottleBin { get; private set; }
+        public static Item Brush { get; private set; }
+        public static Item Candy { get; private set; }
+        public static Item CardboardBox { get; private set; }
         public static Item CashRegister { get; private set; }
+        public static Item CaveDwellerBaby { get; private set; }
+        public static Item ChemicalJug { get; private set; }
+        public static Item Clipboard { get; private set; }
+        public static Item Clock { get; private set; }
+        public static Item ClownHorn { get; private set; }
+        public static Item Cog1 { get; private set; }
+        public static Item ComedyMask { get; private set; }
+        public static Item ControlPad { get; private set; }
+        public static Item Dentures { get; private set; }
+        public static Item DiyFlashBang { get; private set; }
+        public static Item DustPan { get; private set; }
+        public static Item EasterEgg { get; private set; }
+        public static Item EggBeater { get; private set; }
+        public static Item EnginePart1 { get; private set; }
+        public static Item ExtensionLadder { get; private set; }
+        public static Item FancyLamp { get; private set; }
+        public static Item FancyPainting { get; private set; }
+        public static Item FishTestProp { get; private set; }
+        public static Item FlashLaserPointer { get; private set; }
+        public static Item Flashlight { get; private set; }
+        public static Item Flask { get; private set; }
+        public static Item GarbageLid { get; private set; }
+        public static Item GiftBox { get; private set; }
+        public static Item GoldBar { get; private set; }
+        public static Item GunAmmo { get; private set; }
+        public static Item Hairdryer { get; private set; }
+        public static Item Jetpack { get; private set; }
+        public static Item Key { get; private set; }
+        public static Item Knife { get; private set; }
+        public static Item LockPicker { get; private set; }
+        public static Item LungApparatus { get; private set; }
+        public static Item MagnifyGlass { get; private set; }
+        public static Item MapDevice { get; private set; }
+        public static Item MetalSheet { get; private set; }
+        public static Item MoldPan { get; private set; }
+        public static Item Mug { get; private set; }
+        public static Item PerfumeBottle { get; private set; }
+        public static Item Phone { get; private set; }
+        public static Item PickleJar { get; private set; }
+        public static Item PillBottle { get; private set; }
+        public static Item PlasticCup { get; private set; }
+        public static Item ProFlashlight { get; private set; }
+        public static Item RadarBooster { get; private set; }
+        public static Item Ragdoll { get; private set; }
+        public static Item RedLocustHive { get; private set; }
+        public static Item Remote { get; private set; }
+        public static Item Ring { get; private set; }
+        public static Item RobotToy { get; private set; }
+        public static Item RubberDuck { get; private set; }
+        public static Item Shotgun { get; private set; }
+        public static Item Shovel { get; private set; }
+        public static Item SoccerBall { get; private set; }
+        public static Item SodaCanRed { get; private set; }
+        public static Item SprayPaint { get; private set; }
+        public static Item SteeringWheel { get; private set; }
+        public static Item StickyNote { get; private set; }
+        public static Item StopSign { get; private set; }
+        public static Item StunGrenade { get; private set; }
+        public static Item TeaKettle { get; private set; }
+        public static Item ToiletPaperRolls { get; private set; }
+        public static Item Toothpaste { get; private set; }
+        public static Item ToyCube { get; private set; }
+        public static Item ToyTrain { get; private set; }
+        public static Item TragedyMask { get; private set; }
+        public static Item TZPInhalant { get; private set; }
+        public static Item WalkieTalkie { get; private set; }
+        public static Item WeedKillerBottle { get; private set; }
+        public static Item WhoopieCushion { get; private set; }
+        public static Item YieldSign { get; private set; }
+        public static Item ZapGun { get; private set; }
+        public static Item Zeddog { get; private set; }
+
         // todo: add all items or look at automatically generating this somehow so xu doesn't go insasne
-        
+
         internal static void Init()
         {
             List<string> unknownTypes = [];
-            
+
             for (int i = 0; i < NetworkManager.Singleton.NetworkConfig.Prefabs.Prefabs.Count; i++)
             {
                 NetworkPrefab networkPrefab = NetworkManager.Singleton.NetworkConfig.Prefabs.Prefabs[i];
@@ -138,7 +219,7 @@ public static class LethalContent
                     continue;
 
                 _all.Add(grabbable.itemProperties);
-                CodeRebirthLibPlugin.ExtendedLogging($"Found enemy: {grabbable.itemProperties.name}");
+                CodeRebirthLibPlugin.ExtendedLogging($"Found item: {grabbable.itemProperties.name}");
 
                 PropertyInfo property = typeof(Items).GetProperty(grabbable.itemProperties.name.Replace("7", "Seven")); // todo: make this better
                 if (property == null) unknownTypes.Add(grabbable.itemProperties.name);
@@ -148,8 +229,8 @@ public static class LethalContent
                     _vanilla.Add(grabbable.itemProperties);
                 }
             }
-            
-            CodeRebirthLibPlugin.ExtendedLogging($"Unknown enemy types: {string.Join(", ", unknownTypes)}");
+
+            CodeRebirthLibPlugin.ExtendedLogging($"Unknown item types: {string.Join(", ", unknownTypes)}");
         }
     }
 
@@ -172,19 +253,24 @@ public static class LethalContent
         {
             List<string> unknownTypes = [];
             
-            foreach (DungeonFlow flow in new List<DungeonFlow>()) // todo
+            foreach (DungeonFlow flow in RoundManager.Instance.dungeonFlowTypes.Select(i => i.dungeonFlow))
             {
                 _allFlows.Add(flow);
                 CodeRebirthLibPlugin.ExtendedLogging($"Found dungeon flow: {flow.name}");
 
                 PropertyInfo property = typeof(Dungeons).GetProperty(flow.name);
-                if (property == null) unknownTypes.Add(flow.name);
+                if (property == null)
+                {
+                    unknownTypes.Add(flow.name);
+                }
                 else
                 {
                     property.SetValue(null, flow);
                     _vanillaFlows.Add(flow);
                 }
             }
+
+            CodeRebirthLibPlugin.ExtendedLogging($"Unknown dungeon flows: {string.Join(", ", unknownTypes)}");
 
             foreach (DungeonFlow flow in VanillaFlows)
             {
@@ -194,13 +280,13 @@ public static class LethalContent
 
                     foreach (Doorway doorway in doorways)
                     {
-                        if(_vanillaSockets.Contains(doorway.socket)) continue;
+                        if (_vanillaSockets.Contains(doorway.socket))
+                            continue;
+
                         _vanillaSockets.Add(doorway.socket);
                     }
                 }
-            }
-            
-            CodeRebirthLibPlugin.ExtendedLogging($"Unknown enemy types: {string.Join(", ", unknownTypes)}");
+            }            
         }
     }
 }
