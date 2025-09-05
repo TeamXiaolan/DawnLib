c54f4a573b913a3d0d08faa4000a2bce0febd94a
LoafOrc <33192068+LoafOrc@users.noreply.github.com>
2025-09-05 10:02:32 +1000

    oops, add CreateAssetMenu to pricing strategy

--- DawnLib.Dusk/src/API/DuskModConstants.cs
+++ DawnLib.Dusk/src/API/DuskModConstants.cs
@@ -5,7 +5,9 @@ static class DuskModConstants
     internal const string Definitions = $"{MenuName}/Definitions";
     internal const string Achievements = $"{MenuName}/Definitions/Achievements";
     internal const string TerminalPredicates = $"{MenuName}/Terminal Predicates";
+    internal const string PricingStrategies = $"{MenuName}/Pricing Strategies";
 
     internal const int PredicateOrder = -50;
+    internal const int PricingStrategyOrder = -70;
     internal const int DuskModInfoOrder = -10;
 }
--- DawnLib.Dusk/src/API/PricingStrategies/DailyPricingStrategy.cs
+++ DawnLib.Dusk/src/API/PricingStrategies/DailyPricingStrategy.cs
@@ -5,6 +5,7 @@ namespace Dusk;
 
 // would probably be nice to have as an editor tool the ability to see what the prices would be on each day
 // i would have it as like int day1Price, day2Price or smth but there are mods that extend the duration of the quota
+[CreateAssetMenu(menuName = $"{DuskModConstants.PricingStrategies}/Daily Pricing Strategy", fileName = "New Daily Pricing", order = DuskModConstants.PricingStrategyOrder)]
 public class DailyPricingStrategy : DuskPricingStrategy
 {
     [SerializeField, Tooltip("The x axis represents the progress to the deadline. So 1 is company day.")] 
