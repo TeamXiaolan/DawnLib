317771a620898285ad980be296eab3c41b8b92b3
LoafOrc <33192068+LoafOrc@users.noreply.github.com>
2025-09-11 16:51:10 +1000

    refactor item group tagging

---
+++ DawnLib/src/API/Items/AutoTaggers/AutoItemGroupTagger.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace Dawn;
+public class AutoItemGroupTagger(NamespacedKey tag, string itemGroupName) : IAutoTagger<DawnItemInfo>
+{
+
+    public NamespacedKey Tag => tag;
+    public bool ShouldApply(DawnItemInfo info)
+    {
+        if (info.Item.spawnPositionTypes == null)
+            return false;
+
+        return info.Item.spawnPositionTypes.Any(itemGroup => itemGroup != null && itemGroup.name == itemGroupName);
+    }
+}
