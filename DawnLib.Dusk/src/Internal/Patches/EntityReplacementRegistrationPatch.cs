9b2173e7892d1d8c37a53e75cce6429af8471fd6
LoafOrc <33192068+LoafOrc@users.noreply.github.com>
2025-09-18 17:41:03 +1000

    ginaminosauars transpiler

--- DawnLib.Dusk/src/Internal/Patches/EntityReplacementRegistrationPatch.cs
+++ DawnLib.Dusk/src/Internal/Patches/EntityReplacementRegistrationPatch.cs
@@ -1,10 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using Dawn;
+using Dawn.Internal;
+using Dawn.Preloader.Interfaces;
 using HarmonyLib;
 using MonoMod.Cil;
 using MonoMod.RuntimeDetour;
+using UnityEngine;
+using OpCodes = Mono.Cecil.Cil.OpCodes;
 using Random = System.Random;
 
 namespace Dusk.Internal;
@@ -29,6 +34,109 @@ static class EntityReplacementRegistrationPatch
         };
 
         _ = new Hook(AccessTools.DeclaredMethod(typeof(EnemyAINestSpawnObject), "Awake"), OnNestSpawnAwake);
+        
+        // this isn't great, but i don't know a better way to do it?
+        // could maybe do some analysis on the game and then source generate this?
+        IL.EnemyAI.HitEnemy += DynamicallyReplaceAudioClips;
+        IL.EnemyAI.SetEnemyStunned += DynamicallyReplaceAudioClips;
+        
+        IL.BaboonBirdAI.killPlayerAnimation += DynamicallyReplaceAudioClips;
+        IL.BaboonBirdAI.OnCollideWithEnemy += DynamicallyReplaceAudioClips;
+        IL.BaboonBirdAI.OnCollideWithPlayer += DynamicallyReplaceAudioClips;
+        IL.BaboonBirdAI.Update += DynamicallyReplaceAudioClips;
+
+        IL.BushWolfEnemy.OnCollideWithEnemy += DynamicallyReplaceAudioClips;
+        
+        IL.ButlerEnemyAI.ButlerBlowUpAndPop += DynamicallyReplaceAudioClips;
+        IL.ButlerEnemyAI.StabPlayerClientRpc += DynamicallyReplaceAudioClips;
+        IL.ButlerEnemyAI.Update += DynamicallyReplaceAudioClips;
+
+        IL.DocileLocustBeesAI.DaytimeEnemyLeave += DynamicallyReplaceAudioClips;
+        IL.DocileLocustBeesAI.Update += DynamicallyReplaceAudioClips;
+        
+        IL.DoublewingAI.Update += DynamicallyReplaceAudioClips;
+        
+        IL.FlowerSnakeEnemy.MakeChuckleClientRpc += DynamicallyReplaceAudioClips;
+        IL.FlowerSnakeEnemy.SetClingToPlayer += DynamicallyReplaceAudioClips;
+        IL.FlowerSnakeEnemy.SetFlappingLocalClient += DynamicallyReplaceAudioClips;
+        IL.FlowerSnakeEnemy.Start += DynamicallyReplaceAudioClips;
+        IL.FlowerSnakeEnemy.StartLeapOnLocalClient += DynamicallyReplaceAudioClips;
+        IL.FlowerSnakeEnemy.StopClingingOnLocalClient += DynamicallyReplaceAudioClips;
+        IL.FlowerSnakeEnemy.StopLeapOnLocalClient += DynamicallyReplaceAudioClips;
+        
+        IL.MaskedPlayerEnemy.killAnimation += DynamicallyReplaceAudioClips;
+        IL.MaskedPlayerEnemy.SetMaskGlow += DynamicallyReplaceAudioClips;
+        
+        IL.NutcrackerEnemyAI.HitEnemy += DynamicallyReplaceAudioClips;
+        IL.NutcrackerEnemyAI.ReloadGun += DynamicallyReplaceAudioClips;
+        IL.NutcrackerEnemyAI.Update += DynamicallyReplaceAudioClips;
+        
+        IL.RadMechAI.ChangeBroadcastClipClientRpc += DynamicallyReplaceAudioClips;
+        IL.RadMechAI.LateUpdate += DynamicallyReplaceAudioClips;
+        IL.RadMechAI.Stomp += DynamicallyReplaceAudioClips;
+        
+        IL.RedLocustBees.BeesZap += DynamicallyReplaceAudioClips;
+        IL.RedLocustBees.DaytimeEnemyLeave += DynamicallyReplaceAudioClips;
+    }
+    
+    // note!!! this transpiler should only be used on enemy AIs!
+    private static readonly Dictionary<string, Func<EnemyAI, AudioClip, AudioClip>> clipReplacerFunctions = new()
+    {
+        { nameof(EnemyType.hitBodySFX), GenerateAudioClipReplacer(it => it.HitBodySFX) },
+        { nameof(EnemyType.hitEnemyVoiceSFX), GenerateAudioClipReplacer(it => it.HitEnemyVoiceSFX) },
+        { nameof(EnemyType.deathSFX), GenerateAudioClipReplacer(it => it.DeathSFX) }, // isn't used in game??
+        { nameof(EnemyType.stunSFX), GenerateAudioClipReplacer(it => it.StunSFX) }
+    };
+    private static void DynamicallyReplaceAudioClips(ILContext il)
+    {
+        Debuggers.Patching?.Log($"patching: {il.Method.Name} with {nameof(DynamicallyReplaceAudioClips)}");
+        ILCursor c = new ILCursor(il);
+
+        // you probably need to do null/empty array checks as well
+        // this could also probably be cleaned up significantly.
+
+        // evil for loop. loop backwards so emitting doesn't fuck us over later
+        for (c.Index = c.Instrs.Count - 1; c.Index >= 0; c.Index--)
+        {
+            if(c.Next.OpCode != OpCodes.Ldfld) 
+                continue;
+            
+            if(c.Next.MatchLdfld<EnemyType>(nameof(EnemyType.audioClips)))
+            {
+                c.Emit(OpCodes.Ldarg_0);
+                c.EmitDelegate((EnemyAI self, AudioClip[] existingAudioClips) =>
+                {
+                    ICurrentEntityReplacement replacement = (ICurrentEntityReplacement)self;
+                    if (replacement.CurrentEntityReplacement == null) return existingAudioClips;
+                    return ((DuskEnemyReplacementDefinition)replacement.CurrentEntityReplacement).AudioClips;
+                });
+            }
+
+            foreach ((string name, var replacer) in clipReplacerFunctions)
+            {
+                if(!c.Next.MatchLdfld<EnemyType>(name))
+                    continue;
+
+                c.Emit(OpCodes.Ldarg_0);
+                c.EmitDelegate(replacer);
+            }
+        }
+    }
+
+    static Func<EnemyAI, AudioClip, AudioClip> GenerateAudioClipReplacer(Func<DuskEnemyReplacementDefinition, AudioClip> generator)
+    {
+        return (self, existing) =>
+        {
+            ICurrentEntityReplacement replacement = (ICurrentEntityReplacement)self;
+            if (replacement.CurrentEntityReplacement == null) 
+                return existing;
+            
+            AudioClip replacedClip = generator((DuskEnemyReplacementDefinition)replacement.CurrentEntityReplacement);
+            if (!replacedClip)
+                return existing;
+
+            return replacedClip;
+        };
     }
 
     private static void OnNestSpawnAwake(RuntimeILReferenceBag.FastDelegateInvokers.Action<EnemyAINestSpawnObject> orig, EnemyAINestSpawnObject self)
