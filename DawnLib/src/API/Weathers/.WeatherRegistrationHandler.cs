1fd7b2ce1ab90afa3366ff5b0c5005d5da220e82
Xu Xiaolan <126674348+XuuXiaolan@users.noreply.github.com>
2025-10-02 23:14:58 +0100

    Update .WeatherRegistrationHandler.cs

--- DawnLib/src/API/Weathers/.WeatherRegistrationHandler.cs
+++ DawnLib/src/API/Weathers/.WeatherRegistrationHandler.cs
@@ -13,7 +13,7 @@ static class WeatherRegistrationHandler
 
     private static void RegisterWeathers(On.Terminal.orig_Awake orig, Terminal self)
     {
-        foreach (WeatherEffect weatherEffect in TimeOfDayRefs.effects)
+        foreach (WeatherEffect weatherEffect in TimeOfDayRefs.Instance.effects)
         {
             if (weatherEffect.HasDawnInfo())
                 continue;
