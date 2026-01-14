d62e471b559f775b2b15b3df89519cc4442ab8ee
darmuh <darmuh8@gmail.com>
2026-01-14 01:19:56 -0800

    Initial commit with latest version of Pull Request 45

---
+++ DawnLib.Interfaces/src/Interfaces/ITerminal.cs
@@ -0,0 +1,12 @@
+﻿using InjectionLibrary.Attributes;
+
+[assembly: RequiresInjections]
+
+namespace Dawn;
+
+[InjectInterface(typeof(Terminal))]
+public interface ITerminal
+{
+    //Used by TerminalKeywords that accept input to determine the last keyword used
+    string DawnLastCommand { get; set; }
+}
