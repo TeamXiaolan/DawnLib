8419137d2fa721cd3f1adc94f8b1f06947ccd5fb
XuuXiao <ammaroaladel@Gmail.com>
2026-01-20 06:59:09 +0000

    finished work on the dusk terminal commands

--- DawnLib.Dusk/src/API/Definitions/Terminal/DuskTerminalCommandDefinition.cs
+++ DawnLib.Dusk/src/API/Definitions/Terminal/DuskTerminalCommandDefinition.cs
@@ -1,13 +1,58 @@
+using System;
+using System.Collections.Generic;
 using Dawn;
 using UnityEngine;
+using static Dawn.TerminalCommandRegistration;
 
 namespace Dusk;
+
+[Serializable]
+public class QueryCommand
+{
+    [field: SerializeField]
+    public bool Enabled { get; private set; }
+
+    [field: SerializeField]
+    public string QueryDisplayText { get; private set; }
+
+    [field: SerializeField]
+    public string CancelDisplayText { get; private set; }
+
+    [field: SerializeField]
+    public string CancelKeyword { get; private set; }
+
+    [field: SerializeField]
+    public string ContinueKeyword { get; private set; }
+}
 
 [CreateAssetMenu(fileName = "New TerminalCommand Definition", menuName = $"{DuskModConstants.Definitions}/TerminalCommand Definition")]
 public class DuskTerminalCommandDefinition : DuskContentDefinition<DawnTerminalCommandInfo>
 {
     [field: SerializeField]
     public string CommandName { get; private set; }
+
+    [field: SerializeField]
+    [field: TextArea(2, 20)]
+    public string CommandMainText { get; private set; }
+
+    [field: SerializeField]
+    public List<string> CommandKeywordsList { get; private set; }
+
+    [field: SerializeField]
+    [field: Tooltip("Category of the command for internal use.")]
+    public string CommandCategory { get; private set; }
+
+    [field: SerializeField]
+    [field: Tooltip("Description of the command for internal use.")]
+    public string CommandDescription { get; private set; }
+
+    [field: SerializeField]
+    [field: Tooltip("Clear text flags for the command, i.e. which commands clear text on entering.")]
+    public ClearText CommandClearTextFlags { get; private set; }
+
+    [field: SerializeField]
+    [field: Tooltip("Setup a query command that involves a continue and cancel operation to reach the main command text.")]
+    public QueryCommand QueryCommand { get; private set; }
 
     public TerminalCommandConfig Config { get; private set; }
 
@@ -19,6 +64,22 @@ public class DuskTerminalCommandDefinition : DuskContentDefinition<DawnTerminalC
 
         DawnLib.DefineTerminalCommand(TypedKey, CommandName, builder =>
         {
+            builder.SetEnabled(new SimpleProvider<bool>(true));
+            builder.SetMainText(() => CommandMainText);
+            builder.SetKeywords(new SimpleProvider<List<string>>(CommandKeywordsList));
+            builder.SetCategoryName(CommandCategory);
+            builder.SetDescription(CommandDescription);
+            builder.SetClearTextFlags(CommandClearTextFlags);
+            if (QueryCommand.Enabled)
+            {
+                builder.DefineQueryCommand(builder =>
+                {
+                    builder.SetQuery(() => QueryCommand.QueryDisplayText);
+                    builder.SetCancel(() => QueryCommand.CancelDisplayText);
+                    builder.SetCancelWord(QueryCommand.CancelKeyword);
+                    builder.SetContinueWord(QueryCommand.ContinueKeyword);
+                });
+            }
             ApplyTagsTo(builder);
         });
     }
