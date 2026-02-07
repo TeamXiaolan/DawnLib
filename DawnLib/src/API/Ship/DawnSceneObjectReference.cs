0ddfc51029883c1fb40f3211a211f85c279b8887
xntkrnl <maminbors@gmail.com>
2026-02-07 19:25:46 +0300

    DawnSceneObjectReference

---
+++ DawnLib/src/API/Ship/DawnSceneObjectReference.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dawn;
+public class DawnSceneObjectReference : MonoBehaviour
+{
+    public string sceneObjectReferenceSearch;
+    public bool searchInInactive = true;
+    public bool exactMatch = true;
+    public bool keepVisiable = false;
+
+    [Space]
+    public Color hologramColor = Color.cyan;
+    public Color wireframeColor = Color.white;
+
+    [HideInInspector]
+    public string cachedObjectPath = "";
+
+    //[HideInInspector]
+    //public List<Mesh> cachedMeshes = new List<Mesh>();
+
+    //[HideInInspector]
+    //public List<Matrix4x4> cachedTransforms = new List<Matrix4x4>();
+}
